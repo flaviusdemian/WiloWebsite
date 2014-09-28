5bd14aa9bfb9cd3fd717ec468faacb3704b9a7fb
Flavius-Radu Demian <flaviusdemian91@hotmail.com>
2014-09-28 16:21:00 +0300

    added db connections

--- Wilo Fresh Start/Wilo/Wilo/Config.php
+++ Wilo Fresh Start/Wilo/Wilo/Config.php
@@ -1,10 +1,32 @@
 <?php
-
-// Connection's Parameters
-$con=mysqli_connect("localhost","root","","wilo");
-// Check connection
-if (mysqli_connect_errno())
+try 
 {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    $con=mysqli_connect("localhost","root","","wilo");
+    //Check connection
+    if (mysqli_connect_errno())
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+}
+catch (Exception $e) 
+{
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
 ?>
+
+<?php
+  //azure db connection
+  //try 
+  //{
+  //    $con=mysqli_connect("eu-cdbr-azure-north-b.cloudapp.net","beffdfcc82c36e","a0ee51354c57619","Wilo");
+  //     //Check connection
+  //    if (mysqli_connect_errno())
+  //    {
+  //        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  //    }
+  //}
+  //catch (Exception $e) 
+  //{
+  //    echo 'Caught exception: ',  $e->getMessage(), "\n";
+  //}
+  //?>
