5e6ec383e33dcf8b4e425a0232de25b960d7fb2f
fdemian <flavius.demian@avaelgo.ro>
2014-10-02 17:55:07 +0300

    responsive menu on the way

--- Wilo Fresh Start/Wilo/Wilo/js/custom.js
+++ Wilo Fresh Start/Wilo/Wilo/js/custom.js
@@ -1,22 +1,9 @@
 $(document).ready(function () {
-    $(".dropdown ul").wrap("<div />");
 
-    // Notification/inbox dropdown menu
-    $(".dropdown").hover(function () {
-        $(this).find("div").stop(true, true).fadeIn(500);
-    }, function () {
-        $(this).find("div").stop(true, true).fadeOut(500);
-    });
-    
-    $(function () {
-        $("#accordion").accordion({
-            collapsible: true
-        });
-    });
 });
 
 var logicModule = function () {
-    
+
     function loadPage(link) {
         window.location = link;
     }
